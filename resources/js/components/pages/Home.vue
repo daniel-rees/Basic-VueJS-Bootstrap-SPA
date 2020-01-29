1e81873216eae25a68c03abd081715590c2c6954
Daniel Rees <danleyweb@gmail.com>
2020-01-29 10:34:42 +0000

    Add footer with Moment.js copyright auto date

--- resources/js/components/pages/Home.vue
+++ resources/js/components/pages/Home.vue
@@ -15,17 +15,42 @@
           <div class="product-device product-device-2 box-shadow d-none d-md-block"></div>
         </div>
 
+
+        <!-- content -->
+        <div class="container py-5">
+            <div class="row">
+                <div class="col-md-4">
+                  <h2>Heading</h2>
+                  <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                  <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                </div>
+                <div class="col-md-4">
+                  <h2>Heading</h2>
+                  <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                  <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+               </div>
+                <div class="col-md-4">
+                  <h2>Heading</h2>
+                  <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                  <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                </div>
+            </div>
+        </div>
+
+        <footer-nav></footer-nav>
     </div>
 </template>
 
 <script>
     import mainNav from '../partials/MainNav';
+    import footerNav from '../partials/FooterNav';
 
     export default {
         name: "Home",
 
         components: {
-            mainNav
+            mainNav,
+            footerNav
         }
     }
 </script>
---
+++ resources/js/components/partials/FooterNav.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer class="row p-4 bg-dark text-white">
+        <div class="container text-center">
+            <div class="col-sm-12">
+              <p class="mb-1">{{ new Date() | moment("YYYY") }} © Basic VueJS + Bootstrap SPA</p>
+            </div>
+        </div>
+    </footer>
+
+</template>
+
+<script>
+
+export default {
+    name: 'FooterNav'
+}
+</script>
